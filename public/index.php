f1e4c0125781cb7b5cbc64814cef9b2175c55382
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-05-14 10:55:37 +0200

    read with DQL

--- public/index.php
+++ public/index.php
@@ -1,68 +1,25 @@
 <?php
 require_once '../vendor/autoload.php';
+require_once '../src/bootstrap.php';
 
-use Doctrine\DBAL\Tools\DsnParser;
-use Doctrine\DBAL\DriverManager;
 use Onphpoint\WebtCoreDoctrineDbal\Project_Twig_Extension;
 
-$dsnParser = new DsnParser();
-$connectionParams = $dsnParser
-	->parse('pdo-sqlite:///../usarps.sqlite');
+// former query
+// $queryBuilder
+	// ->select('gameround_id', 'rounddate', 'player1_name', 'player1_symbol', 'player2_name', 'player2_symbol')
+	// ->from('gameround')
+	// ->orderBy('rounddate', 'DESC');
 
-$conn = DriverManager::getConnection($connectionParams);
+// $result = $queryBuilder->executeQuery();
 
-
-$queryBuilder = $conn->createQueryBuilder();
-
-
-$queryBuilder
-	->select('gameround_id', 'rounddate', 'player1_name', 'player1_symbol', 'player2_name', 'player2_symbol')
-	->from('gameround')
-	->orderBy('rounddate', 'DESC');
-
-$result = $queryBuilder->executeQuery();
-
-// while ($row = $result->fetchAssociative()) {
-// echo sprintf("Round %d: %s played %s against %s played %s\n", $row['gameround_id'], $row['player1_name'], $row['player1_symbol'], $row['player2_name'], $row['player2_symbol']);
-// }
-
-
-$rounds = array_map(function ($row) {
-
-	// calculate winner
-//	$winner = null;
-	if ($row['player1_symbol'] === $row['player2_symbol']) {
-		$winner = null;
-	} elseif ($row['player1_symbol'] === 'R' && $row['player2_symbol'] === 'S') {
-		$winner = 1;
-	} elseif ($row['player1_symbol'] === 'S' && $row['player2_symbol'] === 'P') {
-		$winner = 1;
-	} elseif ($row['player1_symbol'] === 'P' && $row['player2_symbol'] === 'R') {
-		$winner = 1;
-	} else {
-		$winner = 2;
-	}
-
-	$date = new DateTime();
-	$date->setTimestamp(intval($row['rounddate']));
-	// $date = date_parse($date->format('Y-m-d H:i:s'));
-
-	return [
-		'gameround_id' => $row['gameround_id'],
-		'rounddate' => $date,
-		'player1_name' => $row['player1_name'],
-		'player1_symbol' => $row['player1_symbol'],
-		'player2_name' => $row['player2_name'],
-		'player2_symbol' => $row['player2_symbol'],
-		'winner' => $winner
-	];
-
-}, $result->fetchAllAssociative());
-
+// now as DQL
+$dql = "SELECT g FROM Onphpoint\WebtCoreDoctrineDbal\Models\Gameround g ORDER BY g.rounddate DESC";
+$query = $entityManager->createQuery($dql);
+$result = $query->getResult();
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 $twig->addExtension(new Project_Twig_Extension());
 $template = $twig->load('index.html.twig');
 
-echo $template->render(["gamerounds" => $rounds]);
+echo $template->render(["gamerounds" => $result]);
--- src/models/Gameround.php
+++ src/models/Gameround.php
@@ -1,22 +1,25 @@
 <?php
+
 namespace Onphpoint\WebtCoreDoctrineDbal\Models;
+
 use Doctrine\ORM\Mapping as ORM;
 
 
 #[ORM\Entity]
 #[ORM\Table(name: 'gameround')]
-class Gameround {
+class Gameround
+{
 
-	#[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]
 	private string $player1_name;
 
-	#[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]
 	private string $player2_name;
 
-	#[ORM\Column(type: 'string', length: 1)]
+    #[ORM\Column(type: 'string', length: 1)]
 	private string $player1_symbol;
 
-	#[ORM\Column(type: 'string', length: 1)]
+    #[ORM\Column(type: 'string', length: 1)]
 	private string $player2_symbol;
 
 
@@ -25,46 +28,77 @@ class Gameround {
     #[ORM\GeneratedValue]
     private int|null $gameround_id = null;
 
-	#[ORM\Column(type: 'integer')]
+    #[ORM\Column(type: 'integer')]
 	private int $rounddate;
 
-	/**
-	 * @param string $player1_name
-	 * @param string $player2_name
-	 * @param string $player1_symbol
-	 * @param string $player2_symbol
-	 * @param int $rounddate
-	 */
-	public function __construct(string $player1_name, string $player2_name, string $player1_symbol, string $player2_symbol, int $rounddate) {
-		$this->player1_name = $player1_name;
-		$this->player2_name = $player2_name;
-		$this->player1_symbol = $player1_symbol;
-		$this->player2_symbol = $player2_symbol;
-		$this->rounddate = $rounddate;
-	}
+    /**
+     * @param string $player1_name
+     * @param string $player2_name
+     * @param string $player1_symbol
+     * @param string $player2_symbol
+     * @param int $rounddate
+     */
+    public function __construct(string $player1_name, string $player2_name, string $player1_symbol, string $player2_symbol, int $rounddate)
+    {
+        $this->player1_name = $player1_name;
+        $this->player2_name = $player2_name;
+        $this->player1_symbol = $player1_symbol;
+        $this->player2_symbol = $player2_symbol;
+        $this->rounddate = $rounddate;
+    }
 
 
-	public function getPlayer1Name(): string {
-		return $this->player1_name;
-	}
+    public function getPlayer1Name(): string
+    {
+        return $this->player1_name;
+    }
 
-	public function getPlayer2Name(): string {
-		return $this->player2_name;
-	}
+    public function getPlayer2Name(): string
+    {
+        return $this->player2_name;
+    }
 
-	public function getPlayer1Symbol(): string {
-		return $this->player1_symbol;
-	}
+    public function getPlayer1Symbol(): string
+    {
+        return $this->player1_symbol;
+    }
 
-	public function getPlayer2Symbol(): string {
-		return $this->player2_symbol;
-	}
+    public function getPlayer2Symbol(): string
+    {
+        return $this->player2_symbol;
+    }
 
-	public function getGameroundId(): int {
-		return $this->gameround_id;
-	}
+    public function getGameroundId(): int
+    {
+        return $this->gameround_id;
+    }
 
-	public function getRounddate(): int {
-		return $this->rounddate;
-	}
+    public function getRounddate(): int
+    {
+        return $this->rounddate;
+    }
+
+
+    public function getWinner(): int
+    {
+        // calculate winner
+        if ($this->getPlayer1Symbol() === $this->getPlayer2Symbol()) {
+            return null;
+        } elseif ($this->getPlayer1Symbol() === 'R' && $this->getPlayer2Symbol() === 'S') {
+            return 1;
+        } elseif ($this->getPlayer1Symbol() === 'S' && $this->getPlayer2Symbol() === 'P') {
+            return 1;
+        } elseif ($this->getPlayer1Symbol() === 'P' && $this->getPlayer2Symbol() === 'R') {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public function getDate(): \DateTime
+    {
+        $date = new \DateTime();
+        $date->setTimestamp($this->getRounddate());
+        return $date;
+    }
 }
